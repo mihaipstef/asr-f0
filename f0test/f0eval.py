d44b4c1f928b0fcdcf1af850505aef15a009c797
Mihai Stef <mihai-petru.stef@riverbed.com>
2017-04-27 14:51:10 +0300

    Evaluate with Decission Tree classifier

--- f0test/f0eval.py
+++ f0test/f0eval.py
@@ -5,22 +5,22 @@ from lib.f0 import *
 
 dbPath = "../db/"
 #speaker = "cmu_us_bdl_arctic"
-trainSpeakers_3nn = [ ("cmu_us_slt_arctic", "female"), ("cmu_us_awb_arctic", "male"), ("H26", "child"), ("H27", "child"),
+trainSpeakers_3class = [ ("cmu_us_slt_arctic", "female"), ("cmu_us_awb_arctic", "male"), ("H26", "child"), ("H27", "child"),
 ("H28", "child"), ("H29", "child"), ("H30", "child"), ("H31", "child"), ("H32", "child"), ("H33", "child"),
 ("H34", "child"), ("H35", "child"), ("H36", "child"), ("H37", "child"), ("H38", "child"), ("H39", "child"),
 ("H40", "child"), ("H41", "child"), ("H42", "child"), ("H43", "child"), ("H44", "child") ]
 
-trainSpeakers_2nn = [ ("cmu_us_slt_arctic", "female"), ("cmu_us_awb_arctic", "male") ]
+trainSpeakers_2class = [ ("cmu_us_slt_arctic", "female"), ("cmu_us_awb_arctic", "male") ]
 
-testSpeakers_3nn = [ ("cmu_us_clb_arctic", "female"), ("cmu_us_bdl_arctic", "male"), ("H45", "child"), ("H46", "child"),
+testSpeakers_3class = [ ("cmu_us_clb_arctic", "female"), ("cmu_us_bdl_arctic", "male"), ("H45", "child"), ("H46", "child"),
 ("H47", "child"), ("H48", "child"),("H49", "child") ,("H50", "child"), ("H51", "child"),
 ("H52", "child"), ("H53", "child"),  ("H54", "child"), ("H55", "child"),  ("H56", "child"), ("H57", "child"),
 ("H58", "child"), ("H59", "child"),  ("H60", "child"), ("H61", "child"),  ("H62", "child"), ("H63", "child"),
 ("H64", "child"), ("H64", "child"),  ("H65", "child"), ("H66", "child"),  ("H67", "child"), ("H68", "child"), ("H69", "child") ]
 
-testSpeakers_2nn = [ ("cmu_us_clb_arctic", "female"), ("cmu_us_bdl_arctic", "male") ]
+testSpeakers_2class = [ ("cmu_us_clb_arctic", "female"), ("cmu_us_bdl_arctic", "male") ]
 
-speakers, testSpeakers, trainFunc = trainSpeakers_3nn, testSpeakers_3nn, train3NNClassifier
+speakers, testSpeakers, trainFunc = trainSpeakers_2class, testSpeakers_2class, train2ClassDTClassifier
 
 #Train
 dataFname = "data.json"
@@ -28,25 +28,36 @@ if not os.path.isfile(dataFname):
     computeFeatures(dbPath, speakers,dataFname)
 classifier = trainFunc(dbPath, speakers,dataFname)
 
-#Test
-error = 0
-total = 0
-utt_total = {}
-err = {}
-for (speaker,_class) in testSpeakers:
-    for fname in os.listdir(dbPath+speaker+"/wav"):
-        (speech, f0) = f0estimate(dbPath+speaker+"/wav/"+fname, 0.5)
-        if len(f0) > 0:
-            features = computeTrajectoryStatistics(f0)
-            total = total + 1
-            if _class != classifier.predict(np.array(features).reshape(1,-1))[0]:
-                error = error + 1
-            if _class in utt_total:
-                utt_total[_class] = utt_total[_class]+1
-            else:
-                utt_total[_class] = 1
+if True:
 
-print "Errors: ",error, "No. of utterances: ", total, "Error rate: ", 100 * float(error) / total, "%"
-print "Test utterances stats"
-for c in utt_total:
-    print "{}: {}% ({}/{})".format(c, 100*float(utt_total[c])/total,utt_total[c],total)
+    #Test
+    error = 0
+    total = 0
+    utt_total = {}
+    err = {}
+    for (speaker,_class) in testSpeakers:
+        for fname in os.listdir(dbPath+speaker+"/wav"):
+            (speech, f0) = f0estimate(dbPath+speaker+"/wav/"+fname, 0.5)
+            if len(f0) > 0:
+                features = computeTrajectoryStatistics(f0)
+                total = total + 1
+                est_class = classifier.predict(np.array(features).reshape(1,-1))[0]
+                if _class != est_class:
+                    error = error + 1
+                    if (_class, est_class) in err:
+                        err[(_class,est_class)] = err[(_class,est_class)] + 1
+                    else:
+                        err[(_class,est_class)] = 1
+                if _class in utt_total:
+                    utt_total[_class] = utt_total[_class]+1
+                else:
+                    utt_total[_class] = 1
+
+    print "Errors: ",error, "No. of utterances: ", total, "Error rate: ", 100 * float(error) / total, "%"
+
+    for (c,est) in err:
+        print "{} -> {}: {}%".format(c,est,100*float(err[(c,est)])/utt_total[c])
+
+    print "Test utterances stats"
+    for c in utt_total:
+        print "{}: {}% ({}/{})".format(c, 100*float(utt_total[c])/total,utt_total[c],total)
--- f0test/lib/f0train.py
+++ f0test/lib/f0train.py
@@ -3,52 +3,68 @@ import os
 import json
 
 def computeFeatures(dbPath, speakers, outFname=None):
-	features = {}
+    features = {}
 
-	for speaker, _class in speakers:
-		features[_class] = []
+    for speaker, _class in speakers:
+        features[_class] = []
 
-	for (speaker,_class) in speakers:
-		for fname in os.listdir(dbPath+speaker+"/wav"):
-			(speech, f0) = f0estimate(dbPath+speaker+"/wav/"+fname)
-			if len(f0) > 0:
-				features[_class].append(computeTrajectoryStatistics(f0))
-	if outFname is not None:
-		with open(outFname, 'w') as f:
-			json.dump(features,f)
-	return features
+    for (speaker,_class) in speakers:
+        for fname in os.listdir(dbPath+speaker+"/wav"):
+            (speech, f0) = f0estimate(dbPath+speaker+"/wav/"+fname)
+            if len(f0) > 0:
+                features[_class].append(computeTrajectoryStatistics(f0))
+                if outFname is not None:
+                    with open(outFname, 'w') as f:
+                        json.dump(features,f)
+    return features
 
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn import datasets
 import random
-def train2NNClassifier(dbPath, speakers, fName=None):
-	if fName is None:
-		data = computeFeatures(dbPath, speakers)
-	else:
-		with open(fName, 'r') as f:
-			data = json.load(f)
-	knn = KNeighborsClassifier()
 
-	dataFemale = np.array(data["female"])
-	dataFemaleY = np.array(["female" for i in xrange(dataFemale.shape[0])])
-	dataMale = np.array(data["male"])
-	dataMaleY = np.array(["male" for i in xrange(dataMale.shape[0])])
-	knn.fit(np.concatenate((dataFemale, dataMale)), np.concatenate((dataFemaleY,dataMaleY)))
-	return knn
+def train2ClassClassifier(classifier, dbPath, speakers, fName=None):
+    if fName is None:
+        data = computeFeatures(dbPath, speakers)
+    else:
+        with open(fName, 'r') as f:
+            data = json.load(f)
 
-def train3NNClassifier(dbPath, speakers, fName=None):
-	if fName is None:
-		data = computeFeatures(dbPath, speakers)
-	else:
-		with open(fName, 'r') as f:
-			data = json.load(f)
-	knn = KNeighborsClassifier()
+    dataFemale = np.array(data["female"])
+    dataFemaleY = np.array(["female" for i in xrange(dataFemale.shape[0])])
+    dataMale = np.array(data["male"])
+    dataMaleY = np.array(["male" for i in xrange(dataMale.shape[0])])
+    classifier.fit(np.concatenate((dataFemale, dataMale)), np.concatenate((dataFemaleY,dataMaleY)))
+    return classifier
 
-	dataFemale = np.array(data["female"])
-	dataFemaleY = np.array(["female" for i in xrange(dataFemale.shape[0])])
-	dataMale = np.array(data["male"])
-	dataMaleY = np.array(["male" for i in xrange(dataMale.shape[0])])
-	dataChild = np.array(data["child"])
-	dataChildY = np.array(["child" for i in xrange(dataChild.shape[0])])
-	knn.fit(np.concatenate((dataFemale, dataMale, dataChild)), np.concatenate((dataFemaleY,dataMaleY,dataChildY)))
-	return knn
+def train3ClassClassifier(classifier, dbPath, speakers, fName=None):
+    if fName is None:
+        data = computeFeatures(dbPath, speakers)
+    else:
+        with open(fName, 'r') as f:
+            data = json.load(f)
+
+    dataFemale = np.array(data["female"])
+    dataFemaleY = np.array(["female" for i in xrange(dataFemale.shape[0])])
+    dataMale = np.array(data["male"])
+    dataMaleY = np.array(["male" for i in xrange(dataMale.shape[0])])
+    dataChild = np.array(data["child"])
+    dataChildY = np.array(["child" for i in xrange(dataChild.shape[0])])
+    classifier.fit(np.concatenate((dataFemale, dataMale, dataChild)), np.concatenate((dataFemaleY,dataMaleY,dataChildY)))
+    return classifier
+
+def train2ClassKnnClassifier(dbPath, speakers, fName=None):
+    knn = KNeighborsClassifier()
+    return train2ClassClassifier(knn, dbPath, speakers, fName)
+
+def train3ClassKnnClassifier(dbPath, speakers, fName=None):
+    knn = KNeighborsClassifier()
+    return train3ClassClassifier(knn, dbPath, speakers, fName)
+
+def train2ClassDTClassifier(dbPath, speakers, fName=None):
+    dt = DecisionTreeClassifier()
+    return train2ClassClassifier(dt, dbPath, speakers, fName)
+
+def train3ClassDTClassifier(dbPath, speakers, fName=None):
+    dt = DecisionTreeClassifier()
+    return train3ClassClassifier(dt, dbPath, speakers, fName)
